0cf8e5ac5fd458d984bcf8ad2a6116c32c8b860e
RE43P3R <87203697+ArkAngeL43@users.noreply.github.com>
2022-02-08 12:33:04 -0500

    Delete port-main directory

--- port-main/src/install.sh
+++
@@ -1,9 +0,0 @@
-sudo apt-get install docker 
-sudo apt-get update 
-git clone  https://github.com/mackwic/colored.git
-cd colored 
-docker build -t colored_image .
-docker run --rm -it -v "$PWD":/src -u `id -u`:`id -g` colored_image /bin/bash -c "cargo build"
-echo 'testing'
-docker run --rm -it -v "$PWD":/src -u `id -u`:`id -g` colored_image /bin/bash -c "cargo test"
-
--- port-main/src/test.rs
+++
@@ -1,9 +0,0 @@
-extern crate colored; // not needed in Rust 2018
-
-    use colored::*;
-
-    // test the example with `cargo run --example most_simple`
-    fn main() {
-        // TADAA!
-        println!("{} {} !", "it".green(), "works".blue().bold());
-    }
